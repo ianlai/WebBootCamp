067a4764c68b13b6365f1a07098cec8569990bf9
Ian <ianlai127@gmail.com>
2016-09-28 11:37:24 +0900

    Array iteration.

--- FrontEnd/javascript/array.js
+++ FrontEnd/javascript/array.js
@@ -16,6 +16,7 @@ console.log(colors)
 colors[10] = "black"
 console.log(colors)
 
+console.log("----- pop & push -----")
 //push new item
 colors.push("white")
 console.log(colors)
@@ -23,19 +24,47 @@ console.log(colors)
 //pop item
 console.log(colors.pop())
 
+console.log("----- shift & unshift -----")
 //add new item to the begin (unshift)
 colors.unshift("yellow")
 console.log(colors)
 
-//get item from the begin (shift)
+//get and delete item from the begin (shift)
 colors.shift()
-console.log(colors)
+console.log("colors:" + colors)
 
-//find: index=2
+
+//find or check contain it or not
+console.log("----- indexOf -----")
+//index=2
 console.log(colors.indexOf("green"))
-
-//find: index=-1 (or check contain it or not)
+//index=-1 (or check contain it or not)
 console.log(colors.indexOf("puple"))
 
+
+//subarray 
+console.log("----- slice -----")
+console.log(colors)
 favoriteColors = colors.slice(1,3)
+console.log(colors)  //original array is not effected
 console.log(favoriteColors)
+
+
+//delete  
+console.log("----- splice -----")
+console.log(colors)
+colors.splice(4,6)
+console.log(colors)  //original array is effected
+
+
+console.log("----- iterate -----")
+
+//iterate: for
+for(var i=0; i<colors.length; i++){
+	console.log("[for] " + i + ": " + colors[i]);
+}
+
+//iterate: forEach
+colors.forEach(function(color,index){
+	console.log("[forEach] " + index + ": " + color);
+});
