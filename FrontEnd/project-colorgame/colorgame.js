395a12f4c73baf3d13bb6ec1a4252a75eab7a060
Ian <ian.explore.world@gmail.com>
2016-10-06 01:05:28 +0900

    ColorGame: Added replay function. Beautify.

--- FrontEnd/project-colorgame/colorgame.js
+++ FrontEnd/project-colorgame/colorgame.js
@@ -7,22 +7,29 @@
 // 	"rgb(0, 255, 255)"
 // ]
 var colors = generateRandomColors(6);
+var pickedColor = pickColor();
+
+var h1 = document.querySelector("h1");
+
 var squares = document.querySelectorAll(".square");
 var messageDisplay = document.querySelector("#message");
-var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
-var h1 = document.querySelector("h1");
+
+var resetBtn = document.querySelector("#resetBtn");
 
 colorDisplay.textContent = pickedColor;
 
+setSquaresDisplay();
+
 for(var i = 0; i < squares.length; i++){
-	squares[i].style.background = colors[i];
 
 	squares[i].addEventListener("click", function(){
 		var clickedColor = this.style.background;
+
 		if(clickedColor === pickedColor){
 			messageDisplay.textContent = "Correct!!";
-			changeSquaresColor(pickedColor);
+			resetBtn.textContent = "Replay";
+			changeAllSquaresColor(pickedColor);
 			h1.style.background = clickedColor;
 		}
 		else{
@@ -32,7 +39,27 @@ for(var i = 0; i < squares.length; i++){
 	});
 }
 
-function changeSquaresColor(color){
+resetBtn.addEventListener("click",function(){
+	reset();
+});
+
+function reset(){
+	colors = generateRandomColors(6);
+	
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+
+	setSquaresDisplay();
+	messageDisplay.textContent = "";
+}
+
+function setSquaresDisplay(color){
+	for(var i=0; i<colors.length ; i++){
+		squares[i].style.background = colors[i];
+	}
+}
+
+function changeAllSquaresColor(color){
 	for(var i=0; i<colors.length ; i++){
 		squares[i].style.background=color;
 	}
